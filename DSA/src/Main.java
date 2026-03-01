3d2e3d72cae4e136122558a2a3cbfac4f7509022
Saad Sayyed <saadsyed950@gmail.com>
2026-03-01 09:14:08 +0530

    Bubble sort

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -1,12 +1,23 @@
 import arrays.BinarySearch;
+import arrays.BubbleSort;
 import arrays.LinearSearch;
 
 public static void main(String[] args) {
-    int[] arr = {10, 50, 160, 222, 323};
+    int[] unsortedArr = {5, 3, 8, 1, 2};
     LinearSearch searchLinear = new LinearSearch();
     BinarySearch searchBinary = new BinarySearch();
 
-    int result = searchBinary.binarySearch(arr, 323);
-    System.out.println("Element found at index: " + result);
+    BubbleSort sortBubble = new BubbleSort();
+    printArray(unsortedArr);
+
+    sortBubble.bubbleSort(unsortedArr);
+    printArray(unsortedArr);
+
     }
 
+private static void printArray(int[] arr) {
+    for (int num : arr) {
+        System.out.print(num + " ");
+    }
+    System.out.println();
+}
